6744d680c11f68caa813eefca3c6888129eb5b1f
TagNinr_Nguyen <tagninr.nguyen@gmail.com>
2018-05-28 16:56:19 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -86,66 +86,110 @@ Route::get('search', [
 	'uses' => 'PageController@Search'
 ]);
 
-//Route Admin
+/**
+ * Route Admin
+ */
 Route::group([
-//    'as' => 'backend.',
     'prefix' => 'admin',
     'namespace' => 'Backend',
 ], function () {
 
+    /**
+     * Route Admin/Dashboard
+     */
     Route::get('index', 'DashboardController@index')->name('index');
-    Route::get('bill', 'BillController@index')->name('bill');
-    Route::get('billdetail', 'BillDetailController@index')->name('billdetail');
-    Route::get('brand', 'BrandController@index')->name('brand');
-    Route::get('category', 'CategoryController@index')->name('category');
-    Route::get('customer', 'CustomerController@index')->name('customer');
-    Route::get('user', 'UserController@index')->name('user');
 
+    /**
+     * Route Admin/Bill
+     */
+    Route::group(['prefix' => 'bill'], function () {
+        Route::get('index','BillController@index')->name('indexBill');
+        Route::get('create','BillController@create')->name('createBill');
+        Route::post('store/{id}','BillController@store')->name('storeBill');
+        Route::get('edit/{id}','BillController@edit')->name('editBill');
+        Route::post('update/{id}','BillController@update')->name('updateBill');
+        Route::delete('delete/{id}','BillController@destroy')->name('deleteBill');
+    });
 
-    Route::get('product', 'ProductController@index')->name('product');
-    Route::get('create-product','Product@create')->name('createProduct');
-    Route::post('store-product/{id}','ProductController@store')->name('storeProduct');
-    Route::get('edit-product/{id}','ProductController@edit')->name('editProduct');
-    Route::post('update-product/{id}','ProductController@update')->name('updateProduct');
-    Route::delete('delete-product/{id}','ProductController@destroy')->name('deleteProduct');
+    /**
+     * Route Admin/BillDetail
+     */
+    Route::group(['prefix' => 'billdetail'], function () {
+        Route::get('index','BillDetailController@index')->name('indexBillDetail');
+        Route::get('create','BillDetailController@create')->name('createBillDetail');
+        Route::post('store/{id}','BillDetailController@store')->name('storeBillDetail');
+        Route::get('edit/{id}','BillDetailController@edit')->name('editBillDetail');
+        Route::post('update/{id}','BillDetailController@update')->name('updateBillDetail');
+        Route::delete('delete/{id}','BillDetailController@destroy')->name('deleteBillDetail');
+    });
 
+    /**
+     * Route Admin/Brand
+     */
+    Route::group(['prefix' => 'brand'], function () {
+        Route::get('index','BrandController@index')->name('indexBra');
+        Route::get('create','BrandController@create')->name('createBra');
+        Route::post('store/{id}','BrandController@store')->name('storeBra');
+        Route::get('edit/{id}','BrandController@edit')->name('editBra');
+        Route::post('update/{id}','BrandController@update')->name('updateBra');
+        Route::delete('delete/{id}','BrandController@destroy')->name('deleteBra');
+    });
 
-//    Route::group([
-//        'prefix' => 'bill'
-//    ], function () {
-//        Route::get('index', 'BillController@index')->name('billindex');
-//    });
+    /**
+     * Route Admin/Category
+     */
+    Route::group(['prefix' => 'category'], function () {
+        Route::get('index','CategoryController@index')->name('indexCat');
+        Route::get('create','CategoryController@create')->name('createCat');
+        Route::post('store/{id}','CategoryController@store')->name('storeCat');
+        Route::get('edit/{id}','CategoryController@edit')->name('editCat');
+        Route::post('update/{id}','CategoryController@update')->name('updateCat');
+        Route::delete('delete/{id}','CategoryController@destroy')->name('deleteCat');
+    });
+
+    /**
+     * Route Admin/Customer
+     */
+    Route::group(['prefix' => 'customer'], function () {
+        Route::get('index','CustomerController@index')->name('indexCus');
+        Route::get('create','CustomerController@create')->name('createCus');
+        Route::post('store/{id}','CustomerController@store')->name('storeCus');
+        Route::get('edit/{id}','CustomerController@edit')->name('editCus');
+        Route::post('update/{id}','CustomerController@update')->name('updateCus');
+        Route::delete('delete/{id}','CustomerController@destroy')->name('deleteCus');
+    });
+
+    /**
+     * Route Admin/Product
+     */
+    Route::group(['prefix' => 'product'], function () {
+        Route::get('index','ProductController@index')->name('indexPro');
+        Route::get('create','ProductController@create')->name('createPro');
+        Route::post('store/{id}','ProductController@store')->name('storePro');
+        Route::get('edit/{id}','ProductController@edit')->name('editPro');
+        Route::post('update/{id}','ProductController@update')->name('updatePro');
+        Route::delete('delete/{id}','ProductController@destroy')->name('deletePro');
+    });
+
+    /**
+     * Route Admin/User
+     */
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('index','UserController@index')->name('indexUser');
+        Route::get('create','UserController@create')->name('createUser');
+        Route::post('store/{id}','UserController@store')->name('storeUser');
+        Route::get('edit/{id}','UserController@edit')->name('editUser');
+        Route::post('update/{id}','UserController@update')->name('updateUser');
+        Route::delete('delete/{id}','UserController@destroy')->name('deleteUser');
+    });
+
+//    Route::get('index', [
+//        'as' => 'index',
+//        'uses' => 'DashboardController@index'
+//    ]);
 //
-//    Route::group([
-//        'prefix' => 'brand'
-//    ], function () {
-//        Route::get('index', 'BrandController@index')->name('brandindex');
-//    });
-//
-//    Route::group([
-//        'prefix' => 'category'
-//    ], function () {
-//        Route::get('index', 'CategoryController@index')->name('catindex');
-//    });
-//
-//    Route::group([
-//        'prefix' => 'product'
-//    ], function () {
-//        Route::get('index', 'ProductController@index')->name('proindex');
-//    });
+//    Route::get('product', [
+//        'as' => 'product',
+//        'uses' => 'ProductController@index'
+//    ]);
 });
-
-//Route::get('admin', [
-//    'as' => 'index',
-//    'uses' => 'Backend\DashboardController@index'
-//]);
-//
-//Route::get('admin/product', [
-//    'as' => 'product',
-//    'uses' => 'Backend\ProductController@product'
-//]);
-//
-//Route::get('category', [
-//    'as' => 'category',
-//    'uses' => 'Backend\CategoryController@category'
-//]);
